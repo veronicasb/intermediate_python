e18b771304de85713541a766ffb9d1de4300201a
Veronica Burgess <burgess.veronicas@gmail.com>
2023-11-24 23:24:44 -0700

    Started "Multiprocessing" lecture
    Of "Intermediate Pyton Programming Course" from freeCodeCamp.org

---
+++ intermediate_lecture/multiprocessing.py
@@ -0,0 +1,9 @@
+# creating and starting multiple processes
+
+# sharing data between processes
+
+# recap: using locks to prevent race conditions
+
+# using queues
+
+# using process pool to manage multiple processes
